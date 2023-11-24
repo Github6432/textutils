472e21f65ddca77af7062d93a7ec233ba4537f1d
Github6432 <parasshankhwar2@gmail.com>
2023-11-24 11:57:04 +0530

    Routing Successfylly leard and working properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import React, { useState } from 'react';
 import Alert from './components/Alert';
 import About from './components/About';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
  
@@ -37,14 +37,16 @@ function App() {
   }
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
+    <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <div className="container my-3">
-    <TextForm heading="Enter the text to analyze below" mode={mode}/>
-    <About/>
-    </div>
+        <Alert alert={alert}/>
+        <Routes>
+          <Route path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode}/>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        </div>
+    </Router>
     </> 
   );
 }
