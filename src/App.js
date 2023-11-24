92e7cbc74dc228908d7da5629e62fc5f3d49c748
Github6432 <parasshankhwar2@gmail.com>
2023-11-24 09:08:26 +0530

    For Routing start code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import About from './components/About';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
  
 function App() {
@@ -40,6 +43,7 @@ function App() {
     <Alert alert={alert}/>
     <div className="container my-3">
     <TextForm heading="Enter the text to analyze below" mode={mode}/>
+    <About/>
     </div>
     </> 
   );
